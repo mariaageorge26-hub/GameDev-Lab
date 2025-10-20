2cfdea51efd88c16632cd9308a608cb9f5d08e0f
mariaageorge26-hub <mariaageorge26@gmail.com>
2025-10-20 05:11:40 -0700

    modified13

--- My project1/Assets/Scripts/PlayerControl1.cs
+++ My project1/Assets/Scripts/PlayerControl1.cs
@@ -14,16 +14,18 @@ public class PlayerControl1 : MonoBehaviour
     public float groundCheckRadius;
     public LayerMask whatIsGround;
     private bool grounded;
+     private Animator anim;
+  
 
     void Start()
     {
-        
+         anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(Spacebar))
+        if (Input.GetKeyDown(Spacebar)&& grounded)
     {
         Jump();
     }
@@ -31,10 +33,14 @@ public class PlayerControl1 : MonoBehaviour
     if (Input.GetKey(L))
     {
         GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        
         if (GetComponent<SpriteRenderer>() != null)
         {
             GetComponent<SpriteRenderer>().flipX = true;
         }
+
+        anim.SetFloat("speed",Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
+
     }
 
     if (Input.GetKey(R))
@@ -44,11 +50,19 @@ public class PlayerControl1 : MonoBehaviour
         {
             GetComponent<SpriteRenderer>().flipX = false;
         }
+        anim.SetFloat("speed",Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
+            anim.SetFloat("Height", GetComponent<Rigidbody2D>().velocity.y);
+    anim.SetBool("Grounded", grounded);
+
+
+        
     }
     }
     void Jump()
 {
     GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+
+
 }
 
 
@@ -56,4 +70,6 @@ void FixedUpdate() {
 
     grounded=Physics2D.OverlapCircle(groundCheck.position,groundCheckRadius,whatIsGround);
 }
+
+
 }
